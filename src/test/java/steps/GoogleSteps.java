c8679371ae49b04cd102711dd015afdb08818eac
Javier Ramos <jaramosperez85@gmail.com>
2023-04-22 11:21:33 -0400

    ADD Google Steps

---
+++ src/test/java/steps/GoogleSteps.java
@@ -0,0 +1,23 @@
+package steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class GoogleSteps {
+    
+    @Given("^I am on the Google search page$")
+    public void iAmOnTheGoogleSearchPage() {
+        //TODO
+    }
+
+    @When("^I do a search on Google$")
+    public void iDoASearchOnGoogle() {
+        //TODO
+    }
+
+    @Then("^I get expected results$")
+    public void iGetExpectedResults() {
+        //TODO
+    }
+}
